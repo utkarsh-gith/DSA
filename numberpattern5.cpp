cec656f5512a813f01949630e8e0b736ac3a2f95
utkarsh-gith <149144723+utkarsh-gith@users.noreply.github.com>
2024-01-08 19:44:26 +0530

    Update numberpattern5.cpp

--- numberpattern5.cpp
+++ numberpattern5.cpp
@@ -30,3 +30,37 @@ int main()
 3       4       5
 4       5       6       7
 */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    cout<<"Enter number of rows"<<endl;
+    cin>>n;
+    cout<<endl;
+    while(i<=n)
+    {
+        int j=1;
+        int count=i;
+        while(j<=i)
+        {
+            cout<<count<<"\t"; 
+            count--;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    cout<<endl;
+    return 0;
+}
+
+/*
+1
+2       1
+3       2       1
+4       3       2       1
+
+*/
