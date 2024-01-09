305cbbfb5c078f6eb22822d1b0f1e5f8c66f32e3
utkarsh-gith <149144723+utkarsh-gith@users.noreply.github.com>
2024-01-09 17:41:50 +0530

    Update characterPattern2.c

--- characterPattern2.c
+++ characterPattern2.c
@@ -67,3 +67,38 @@ B       C
 D       E       F
 G       H       I       J
 */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    cout<<"Enter number of rows"<<endl;
+    cin>>n;
+    cout<<endl;
+    while(i<=n)
+    {
+        int j=1;
+        
+        while(j<=i)
+        {
+            cout<<char('A'+j+i-2)<<"\t"; 
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    cout<<endl;
+    return 0;
+}
+
+/*
+Enter number of rows
+4
+
+A
+B       C
+C       D       E
+D       E       F       G
+*/
