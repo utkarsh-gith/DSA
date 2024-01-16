bfa926c4ab3ee27b441a210ce3d6e853a7323109
Utkarsh Pratap Singh <149144723+utkarsh-gith@users.noreply.github.com>
2024-01-16 19:46:04 +0530

    Create numberOf1Bits.cpp

---
+++ numberOf1Bits.cpp
@@ -0,0 +1,25 @@
+// Number of 1 Bits
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number to be checked:";
+    cin>>n;
+    int s=0;
+    while(n!=0)
+    {
+        if(n&1)
+        s++;
+        n=n>>1;
+    }
+    cout<<"Number of 1 Bits:"<<s<<endl;
+}
+
+
+/*
+Enter the number to be checked:11
+Number of 1 Bits:3
+*/
