27cb242de4e724085c1a9e12e634931a2600987e
Utkarsh Pratap Singh <singhup0911@gmail.com>
2024-05-12 17:27:07 +0530

    LL

--- Arrays/swapAlternate.cpp
+++ Arrays/swapAlternate.cpp
@@ -1,57 +1,40 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-// void swap1(int arr[],int n)
-// {
-//     for(int i=0;i<n-1;i=i+2)
-//     {
-//         swap(arr[i],arr[i+1]); //built in function
-//         // int temp = arr[i];
-//         // arr[i]=arr[i+1];
-//         // arr[i+1]=temp;
-//     }
-// }
-
-// void create(int arr[],int n)
-// { 
-//     cout<<endl<<"Enter array elements: "<<endl;
-//     for(int i=0;i<n;i++)
-//         cin>>arr[i];
-// }
-
-// void display(int arr[],int n)
-// {
-//     cout<<endl<<"Array Elements:"<<endl;
-//     for(int i=0;i<n;i++)
-//         cout<<arr[i]<<"\t";
-//     cout<<endl;
-// }
-
-// int main()
-// {
-//     int arr[20],n;
-//     cout<<endl<<endl<<"Enter array size: ";
-//     cin>>n;
-//     create(arr,n);
-//     display(arr,n);
-//     swap1(arr,n);
-//     display(arr,n);
-//     return 0;
-// }
-
- vector<int> nums = {1, 2, 3, 4};
-    
-    int n = nums.size();
-    nums.resize(2 * n); // Resize the vector to double its size
-
-    // Copy elements to the end of the vector
-    for(int i = 0; i < n; i++)
-        nums[i + n] = nums[i];
-
-    // Output the contents of the vector
-    for(int i = 0; i < nums.size(); i++) {
-        cout << nums[i] << endl;
+void swap1(int arr[],int n)
+{
+    for(int i=0;i<n-1;i=i+2)
+    {
+        swap(arr[i],arr[i+1]); //built in function
+        // int temp = arr[i];
+        // arr[i]=arr[i+1];
+        // arr[i+1]=temp;
     }
+}
 
+void create(int arr[],int n)
+{ 
+    cout<<endl<<"Enter array elements: "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+}
+
+void display(int arr[],int n)
+{
+    cout<<endl<<"Array Elements:"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<"\t";
+    cout<<endl;
+}
+
+int main()
+{
+    int arr[20],n;
+    cout<<endl<<endl<<"Enter array size: ";
+    cin>>n;
+    create(arr,n);
+    display(arr,n);
+    swap1(arr,n);
+    display(arr,n);
     return 0;
+}
---
+++ Linked_List/singlyLL.cpp
@@ -0,0 +1,158 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node * &head,int d){
+    Node *newNode = new Node(d);
+    newNode->next = head;
+    head = newNode;
+}
+void insertAtTail(Node * &head,int d){
+    Node *newNode = new Node(d);
+    Node *temp = head;
+    if(temp==NULL){
+        insertAtHead(head,d);
+    }
+    else{
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void insertAtPos(Node * &head,int d,int pos){
+    if(pos==1){
+        insertAtHead(head,d);
+    }
+    else{
+        Node *newNode = new Node(d);
+        Node *temp = head;
+        int count = 1;
+        while(count<pos-1 && temp->next!=NULL){
+            temp=temp->next;
+            count++;
+        }
+        if(count<pos-1)
+            cout<<"Invalid Position"<<endl;
+        else{
+            newNode->next=temp->next;
+            temp->next=newNode;
+        }
+    }
+}
+
+void deleteAtHead(Node * &head){
+    if(head==NULL){
+        cout<<"Empty List"<<endl;
+    }
+    else{
+        cout<<"Deleted element = "<<head->data<<endl;
+        head=head->next;
+    }
+}
+void deleteAtTail(Node * &head){
+    if(head==NULL){
+        cout<<"Empty List"<<endl;
+    }
+    else if(head->next==NULL){
+        cout<<"Element Deleted = "<<head->data<<endl;
+        head=NULL;
+    }
+    else{
+        Node *temp = head;
+        Node *prev = head;
+        while(temp->next!=NULL){
+            prev=temp;
+            temp=temp->next;
+        }
+        cout<<"Element Deleted = "<<temp->data<<endl;
+        prev->next=NULL;
+        delete(temp);
+    }
+}
+void deleteAtPos(Node * &head,int pos){
+    if(head==NULL){
+        cout<<"Empty List"<<endl;
+    }
+    else if(pos==1){
+        deleteAtHead(head);
+    }
+    else{
+        Node *temp = head;
+        Node *prev = head;
+        int c=1;
+        while(c<pos && temp->next!=NULL){
+            prev = temp;
+            temp = temp->next;
+            c++;
+        }
+            if(c!=pos){
+                cout<<"Invalid Position"<<endl;
+            }
+            else{
+                cout<<"Elemnt Deleted = "<<temp->data;
+                prev->next=temp->next;
+            }
+    }
+}
+
+void print(Node * &head){
+    Node *temp = head;
+    cout<<endl<<"Linked list elements:"<<endl;
+    while(temp!=NULL){
+        cout<<temp->data<<"\t";
+        temp = temp->next;
+    }
+    cout<<endl; 
+}
+
+int main(){
+    Node *head = NULL;
+    int n,ele,pos;
+    while(1){
+        cout<<endl<<"Choose Options:"<<endl;
+        cout<<"1. Insert at Head  2. Insert at End  3. Insert at Pos  "<<endl;;
+        cout<<"4. Delete at Head  5. Delete at End  6. Delete at Pos  "<<endl;
+        cout<<"7.Print  8.Exit"<<endl;
+        cout<<"Choice: ";
+        cin>>n;
+        switch(n){
+            case 1: cout<<"Enter element: ";
+                    cin>>ele;
+                    insertAtHead(head,ele);
+                    break;
+            case 2: cout<<"Enter element: ";
+                    cin>>ele;
+                    insertAtTail(head,ele);
+                    break;
+            case 3: cout<<"Enter element: ";
+                    cin>>ele;
+                    cout<<"Enter Position: ";
+                    cin>>pos;
+                    insertAtPos(head,ele,pos);
+                    break; 
+            case 4: deleteAtHead(head);
+                    break; 
+            case 5: deleteAtTail(head);
+                    break; 
+            case 6: cout<<"Enter Position: ";
+                    cin>>pos;
+                    deleteAtPos(head,pos);
+                    break;  
+            case 7: print(head);
+                    break;
+            case 8: exit(0);
+            default: cout<<"Invalid Choice"<<endl;
+        }
+    }
+    return 0;
+}
